ba2219e644d749e3e89219a9a3c4cef4db22d43e
wkrmnn <anastasia.dob2007@gmail.com>
2024-02-13 00:12:21 +0300

    my file

--- scratch.py
+++ scratch.py
@@ -1,1 +1,26 @@
-print('hello world')
+class Studentik():
+    fio = ''
+    score = 0
+    clas = ''
+    id = 0
+
+students = []
+f = open('students.csv')
+for i in range(10):
+    students.append(Studentik())
+    s = f.readline().split(',')
+    students[i].fio = s[1]
+    students[i].id = s[0]
+    students[i].score = s[4]
+    students[i].clas = s[3]
+
+for i in range(1, len(students)):
+    k = students[i]
+    j = i - 1
+    while j >= 0 and students[j].score < k.score:
+        students[j + 1] = students[j]
+        j -= 1
+    students[j + 1] = k
+
+for i in range(3):
+    print(f"{i+1} место: {students[i].fio}")
