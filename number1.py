d9c7add1394dc37ae38e291aad83429e3999aee5
wkrmnn <anastasia.dob2007@gmail.com>
2024-03-05 12:10:43 +0300

    1 commit

---
+++ number1.py
@@ -0,0 +1,43 @@
+class Studentik():
+    fio = ''
+    score = 0
+    clas = ''
+    id = 0
+    title = ''
+
+j = 0
+students = []
+f = open('students.csv')
+name = f.readline().strip()
+for i in f:
+    students.append(Studentik())
+    s = i.strip().split(',')
+    students[i].fio = s[1]
+    students[i].id = s[0]
+    students[j].title = s[2]
+    students[i].score = s[4]
+    students[i].clas = s[3]
+    j+=1
+
+for i in students:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in students:
+            if j.clas == i.clas and j.score!='None':
+                k+=1
+                s+= int(j.score)
+        i.score = f'{s/k:.3f}'
+fil = open('students_new.csv', 'w')
+print(s, file = fil)
+for i in students:
+    print(i.id,',',i.fio,',',i.title,',', i.clas,',', i.score,',',file = fil)
+
+ff='Хадаров Владимир Валериевич'
+
+for i in range(len(students)):
+    if students[i].fio == ff:
+        print('Ты получил:', students[i].score,', за проект -', students[i].id)
+        break
+
+fil.close()
